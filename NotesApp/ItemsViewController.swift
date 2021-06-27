413a7eab487ef470e4edde5887c5b5e73ef6ffab
Afrizzal P. Pratama <apratama8@gmail.com>
2021-06-27 21:39:37 +0700

    Fix UITableViewDataSource

--- NotesApp/ItemsViewController.swift
+++ NotesApp/ItemsViewController.swift
@@ -1,13 +1,9 @@
-//
-//  ItemsViewController.swift
-//  NotesApp
-//
-//  Created by Afrizzal on 25/06/21.
-//
-
 import UIKit
 
-class ItemsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ItemsViewController: UIViewController,
+            UITableViewDelegate,
+            UITableViewDataSource,
+            UITextFieldDelegate {
 
     @IBOutlet weak var listNameLabel: UILabel!
     @IBOutlet weak var itemTextField: UITextField!
@@ -21,7 +17,35 @@ class ItemsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         super.viewDidLoad()
 
         listNameLabel.text = AppData.sharedInstance.currentLST[curListInt].listName
+        
+        
+        itemTextField.returnKeyType = UIReturnKeyType.done
+        itemTextField.delegate = self
     }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let newItem = ItemClass (inpItemName: textField.text!,
+                                 inpItemTime: Date(),
+                                 inpItemPurchased: false)
+        
+        textField.text = ""
+        
+        AppData.sharedInstance.currentLST[curListInt].listItems.append(newItem)
+        itemsTableView.reloadData()
+        
+        ReadWriteOnDisk.writeData()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    
     
     // TABLE VIEW
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -39,10 +63,10 @@ class ItemsViewController: UIViewController, UITableViewDelegate, UITableViewDat
             cell.backgroundColor = UIColor.darkGray
             cell.textLabel?.textColor = UIColor.lightGray
             cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
-            cell.accessoryType = UITableViewCellAccessoryType.checkmark
+            cell.accessoryType = UITableViewCell.AccessoryType.checkmark
             // cross it off attributedString
             let attrStr = NSMutableAttributedString (string: thisItem.itemName)
-            attrStr.addAttribute(NSAttributedStringKey.strikethroughStyle,
+            attrStr.addAttribute(NSAttributedString.Key.strikethroughStyle,
                                  value: 2,
                                  range: NSMakeRange(0, attrStr.length))
             cell.textLabel?.attributedText = attrStr
@@ -51,9 +75,9 @@ class ItemsViewController: UIViewController, UITableViewDelegate, UITableViewDat
             cell.backgroundColor = UIColor.white
             cell.textLabel?.textColor = UIColor.black
             cell.textLabel?.font = UIFont.systemFont(ofSize: 16)
-            cell.accessoryType = UITableViewCellAccessoryType.none
+            cell.accessoryType = UITableViewCell.AccessoryType.none
             let attrStr = NSMutableAttributedString (string: thisItem.itemName)
-            attrStr.addAttribute(NSAttributedStringKey.strikethroughStyle,
+            attrStr.addAttribute(NSAttributedString.Key.strikethroughStyle,
                                  value: 0,
                                  range: NSMakeRange(0, attrStr.length))
             cell.textLabel?.attributedText = attrStr
@@ -62,6 +86,48 @@ class ItemsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         return cell
     }
     
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        AppData.sharedInstance.currentLST[curListInt].listItems[indexPath.row].itemPurchased =
+        !AppData.sharedInstance.currentLST[curListInt].listItems[indexPath.row].itemPurchased
+        
+        itemsTableView.reloadData()
+        
+        ReadWriteOnDisk.writeData()
+        
+        // add the time of the latest edit
+    }
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String?
+    {
+        return "Delete?"
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        AppData.sharedInstance.currentLST[curListInt].listItems.remove(at: indexPath.row)
+        
+        ReadWriteOnDisk.writeData()
+        
+        tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
     @IBAction func goBackAction(_ sender: Any) {
         
         self.dismiss(animated: true, completion: nil)
@@ -71,4 +137,3 @@ class ItemsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
     }
 }
-
--- NotesApp/ListsViewController.swift
+++ NotesApp/ListsViewController.swift
@@ -1,139 +1,151 @@
-//
-//  ListsViewController.swift
-//  NotesApp
-//
-//  Created by Afrizzal on 05/06/21.
-//
-
 import UIKit
 
-class ListsViewController: UIViewController {
-
+class ListsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var profileButton: UIButton!
     
     @IBOutlet weak var listsTableView: UITableView!
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        listsTableView.reloadData()
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
+        
         self.readData()
     }
-        
-        func readData ()
-        {
-            ReadWriteOnDisk.readUser()
-            if ( AppData.sharedInstance.curUser == nil)
-            {
-                // the very first time anyone uses this app
-                AppData.sharedInstance.curUser = UserClass (inpName: "Me",
-                                                            inpEmail: "defEmail",
-                                                            inpUid: "defUid")
-                PrepareFirstLists.prepare()
-                
-                ReadWriteOnDisk.writeData()
-                ReadWriteOnDisk.writeUser()
-            }
-            else
-            {
-                ReadWriteOnDisk.readData()
-                AppData.sharedInstance.currentLST = AppData.sharedInstance.offlineLST
-            }
-        }
-        
-        
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            
-            return AppData.sharedInstance.currentLST.count;
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            
-            let cell = tableView.dequeueReusableCell(withIdentifier: "listsCell", for: indexPath)
-            
-            let thisList = AppData.sharedInstance.currentLST[indexPath.row] as NotesAppListClass
-            
-            
-            cell.textLabel?.text = thisList.listName
-            cell.detailTextLabel?.text = String(thisList.listItems.count) + " for " + thisList.listOwner.name
-            
-            return cell
-        }
+    
 
-        
-        
-        
-        func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-            return true
-        }
-        
-        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-            
-            AppData.sharedInstance.currentLST.remove(at: indexPath.row)
-            ReadWriteOnDisk.writeData()
-            
-            
-            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
-        }
-        
-        func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
-            return "Delete?"
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        @IBAction func newListAction(_ sender: Any) {
-            let alert = UIAlertController (title: "New List",
-                                            message: "Please enter the name of your new list",
-                                            preferredStyle: UIAlertController.Style.alert)
-            
-            alert.addTextField { (textField) in
-                textField.placeholder = "new list"
-                textField.font = UIFont.systemFont(ofSize: 22)
-                textField.textAlignment = NSTextAlignment.center
-            }
-            
-            alert.addAction(UIAlertAction(title: "Save",
-                                          style: UIAlertAction.Style.default,
-                                          handler:
-                { (myAlert) in
-                    self.newListSave(inpName: alert.textFields![0].text!)
-                }))
-            
-            alert.addAction(UIAlertAction(title: "Cancel",
-                                          style: UIAlertAction.Style.cancel,
-                                          handler:nil ))
-            
-            self.present(alert, animated: true, completion: nil)
-        }
-        
-
-        func newListSave (inpName: String)
+    func readData ()
+    {
+        ReadWriteOnDisk.readUser()
+        if ( AppData.sharedInstance.curUser == nil)
         {
-            let newList = NotesAppListClass(inpListName: inpName,
-                                           inpOwner: AppData.sharedInstance.curUser!,
-                                           inpListItems: [])
-            
-            AppData.sharedInstance.currentLST.append(newList)
-            
-            listsTableView.reloadData()
+            // the very first time anyone uses this app
+            AppData.sharedInstance.curUser = UserClass (inpName: "Me",
+                                                        inpEmail: "defEmail",
+                                                        inpUid: "defUid")
+            PrepareFirstLists.prepare()
             
             ReadWriteOnDisk.writeData()
+            ReadWriteOnDisk.writeUser()
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        @IBAction func profileAction(_ sender: Any) {
+        else
+        {
+            ReadWriteOnDisk.readData()
+            AppData.sharedInstance.currentLST = AppData.sharedInstance.offlineLST
         }
     }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return AppData.sharedInstance.currentLST.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listsCell", for: indexPath)
+        
+        let thisList = AppData.sharedInstance.currentLST[indexPath.row] as NotesAppListClass
+        
+        
+        cell.textLabel?.text = thisList.listName
+        cell.detailTextLabel?.text = String(thisList.listItems.count) + " for " + thisList.listOwner.name
+        
+        return cell
+    }
+
+    
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        AppData.sharedInstance.currentLST.remove(at: indexPath.row)
+        ReadWriteOnDisk.writeData()
+        
+        
+        tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+    }
+    
+    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Delete?"
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "toItemsSegue_id", sender: indexPath)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        let itemsCtrl : ItemsViewController = segue.destination as! ItemsViewController
+        itemsCtrl.curListInt = (sender as! IndexPath).row
+    }
+    
+    
+    
+    
+    
+    @IBAction func newListAction(_ sender: Any) {
+        let alert = UIAlertController (title: "New List",
+                                        message: "Please enter the name of your new list",
+                                        preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "new list"
+            textField.font = UIFont.systemFont(ofSize: 22)
+            textField.textAlignment = NSTextAlignment.center
+        }
+        
+        alert.addAction(UIAlertAction(title: "Save",
+                                      style: UIAlertAction.Style.default,
+                                      handler:
+            { (myAlert) in
+                self.newListSave(inpName: alert.textFields![0].text!)
+            }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel",
+                                      style: UIAlertAction.Style.cancel,
+                                      handler:nil ))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+
+    func newListSave (inpName: String)
+    {
+        let newList = NotesAppListClass(inpListName: inpName,
+                                       inpOwner: AppData.sharedInstance.curUser!,
+                                       inpListItems: [])
+        
+        AppData.sharedInstance.currentLST.append(newList)
+        
+        listsTableView.reloadData()
+        
+        ReadWriteOnDisk.writeData()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func profileAction(_ sender: Any) {
+    }
+}
